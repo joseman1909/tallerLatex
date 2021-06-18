863b383868cb789afd9421e7d57d29d84bc1f573
joseman1909 <85945690+joseman1909@users.noreply.github.com>
2021-06-17 20:03:04 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+# Problema 1.
+
+base = float(input(("Ingrese la medida de la base del triangulo (cm): ")))
+altura = float(input(("Ingrese la medida de la altura del triangulo (cm): ")))
+
+area = base * altura / 2
+
+print("El area del triangulo es de: %s cm" % area)
+
