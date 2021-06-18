67229ebb12d96a5c64f216c506650fc71babfb0a
joseman1909 <85945690+joseman1909@users.noreply.github.com>
2021-06-17 20:00:38 -0500

    Delete ejercicio2.py

--- ejercicio2.py
+++
@@ -1,9 +0,0 @@
-# Problema 2.
-
-parcial1 = float(input("Ingrese la nota del primer parcial: "))
-parcial2 = float(input("Ingrese la nota del segundo parcial: "))
-taller = float(input("Ingrese la nota del taller: "))
-proyecto = float(input("Ingrese la nota del proyecto: "))
-notafinal = float(parcial1 * 0.25)+(parcial2 * 0.25)+(taller*0.2)+(proyecto*0.3)
-
-print("Tu nota final es: %s" % notafinal)
